413dd972b7dee78a5ca468491bc7f9b8a4279ca0
Otavio <otavio.zerotwo@gmail.com>
2026-01-28 21:53:59 -0300

    feat: add styles.ts remove FormVagas.module.css

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,5 @@
 import { FormEvent, useState } from 'react'
-
-import styles from './FormVagas.module.css'
+import { ButtonStyles, FormStyles, InputStyles } from './styles'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +14,14 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <FormStyles onSubmit={aoEnviarForm}>
+      <InputStyles
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <ButtonStyles type="submit">Pesquisar</ButtonStyles>
+    </FormStyles>
   )
 }
 export default FormVagas
